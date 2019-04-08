de3e881b95604f3e75a66d9c800acbaf25919b20
ERICDONKEY <eric19960124@gmail.com>
2019-04-08 19:49:13 -0400

    modify MLP framework with detail

--- MLP.py
+++ MLP.py
@@ -34,40 +34,124 @@ class MLP(object):
         self.weights = [np.random.randn(y, x) 
                         for x, y in zip(sizes[:-1], sizes[1:])]
     
-    def feeforward(self, a):
-        # do something interesting
+    def feedforward(self, a):
+        """feedforward function will return the output of this neural network.
+        and a is the input of this network."""
+        w_hidden = self.weights[:-1]
+        b_hidden = self.biases[:,-1]
+        if w_hidden == []:
+            pass
+        else:
+            for w, b in zip(w_hidden, b_hidden):
+                a = Relu(np.dot(w, a) + b)
+        a = Softmax(np.dot(self.weights[-1], a) + self.biases[-1])
         return a
-    def SGD(self, training_data, training_results, epochs, mini_batch_size, learning_rate, test_data = None):
-        #do something interesting here
-    
-    def update_mini_batch(self, mini_batch, learning_rate):
-        #do something interesting here
+
+            
+    def SGD(self, train_data, train_results, epochs, mini_batch_size, learning_rate, test_data = None, test_results = None):
+        """Train this neural network using mini-batch stochastic gradient descent. 
+        training_data is list of ndarray. If ``test_data`` is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially.   """
+        if test_data: n_test = len(test_data)
+        n = len(train_data)
+        for j in range(epochs):
+            train_data_shuffled, train_results_shuffled = shuffleTogether(train_data, train_results)
+            mini_batches_data = [train_data_shuffled[k:k+mini_batch_size] 
+                                 for k in range(0, n, mini_batch_size)]
+            mini_batches_results = [train_results_shuffled[k:k+mini_batch_size] 
+                                 for k in range(0, n, mini_batch_size)]
+            for mini_batch_data, mini_batch_results in zip(mini_batches_data, mini_batches_results): 
+                self.update_mini_batch(mini_batch_data, mini_batch_results, learning_rate)
+                if test_data:
+                    print("Epoch {0} Complete with accuracy: {1}/{2}".format(j, self.evaluate(test_data, test_results), n_test))
+                else:
+                    print("Epoch {0} Complete".format(j))
+
+
+    def update_mini_batch(self, mini_batch_data, mini_batch_results, learning_rate):
+        n = len(mini-batch)
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        for x, y in zip(mini_batch_data, mini_batch_results):
+            delta_nabla_w, delta_nabla_b = self.backprop(x, y)
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+        self.weights = [w - (learning_rate/n)*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases  = [b - (learning_rate/n)*nb for b, nb in zip(self.biases, nabla_b)] 
+
 
     def backprop(self, x, y):
         """x is trainging example. y is the label.
-           backprop will return two lists(nabla_b, nabla_w) of ndarrays each which contains derivative wrt specific weight or bias,
+           backprop will return two lists(nabla_w, nabla_b) of ndarrays each which contains derivative wrt specific weight or bias,
            similar to self.weights and self.biases
-           """
-        #do something interesting here
-    
-    def evaluate(self, test_data, test_result):
-        #do something interesting here
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        Activation = x
+        Activations = [x]
+        zs = []
+        ###feedforward part
+        w_hidden = self.weights[:-1]
+        b_hidden = self.biases[:,-1]
+        if w_hidden == []:
+            pass
+        else:
+            for w, b in zip(w_hidden, b_hidden):
+                z = np.dot(w, Activation) + b
+                zs.append(z)
+                Activation = Relu(z)
+                Activations.append(Activation)
+        z = np.dot(self.weights[-1], z) + self.biases[-1]
+        zs.append(z)
+        Activation = Softmax(z)
+        Activations.append(Activation)  
+        ###backforward part 
+        delta = self.cost_derivative(Activation, y)
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, Activations[-2].transpose())
+
+        for l in range(2,self.num_layers):
+            z = zs[-l]
+            zp = d_Relu(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * zp
+            nabla_b[-l] = delta
+            nable_w[-l] = np.dot(delta, Activations[-l-1].transpose())
+        return (nabla_w, nabla_b)
+
+    def evaluate(self, test_data, test_results):
+        tests = [(np.argmax(self.feedforward(x)), np.argmax(y)) for x, y in zip(test_data, test_results)]
+        return sum(int(x == y) for (x, y) in tests)
+
 
     def cost_derivative(self, output_activations, y):
-        #do something interesting here
+        return -1. * y * (1. - output_activations)
 
+        
 ###helper functions
-    def sigmoid(z):
-        #do something interesting here
+def Softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x/np.sum(e_x, axis=0)
 
-    def d_sigmoid(z):
-        #do something interesting here
+def dSoftmax(x):
+    return Softmax(x) * (1 - Softmax(x))    
 
-    def relu(z):
-        #do something interesting here
+def Relu(x):
+    return x * (x > 0)
 
-    def d_relu(z):
-        #do something interesting here        
+def d_Relu(x):
+    return 1. * (x > 0)  
+
+def shuffleTogether(A, B):
+    if len(A) != len(B):
+        raise Exception("Lengths don't match")
+    indexes = list(range(len(A)))
+    random.shuffle(indexes)
+    A_shuffled = [A[i] for i in indexes]    
+    B_shuffled = [B[i] for i in indexes]
+    return A_shuffled, B_shuffled
 
 
 
